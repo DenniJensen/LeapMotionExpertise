9586a6b65138c8c8e78a7c92d8d49c375b7ae9ab
Dennis Hägler <d.haegler@hotmail.de>
2014-01-24 12:19:54 +0100

    Connecting PApplet with system logic

--- LeapMotionMain.java
+++ LeapMotionMain.java
@@ -8,6 +8,7 @@ public class LeapMotionMain {
 	public static void main(String[] args) {
 		MyProcessingSketch sketch = new MyProcessingSketch();
 		PApplet.main(new String[]{"--present", "MyProcessingSketch"});
+		sketch.setField(Field.BOTTOM_RIGHT);
 		ControlPanel ceranControl = new ControlPanel();
 		MyLeapMotionController controller = new MyLeapMotionController(ceranControl);
 		System.out.println("Press Enter to quit...");
--- MyLeapMotionController.java
+++ MyLeapMotionController.java
@@ -4,7 +4,7 @@ import com.leapmotion.leap.Controller;
  * Created by dennis on 1/24/14.
  */
 public class MyLeapMotionController extends Controller {
-	
+
 	public MyLeapMotionController(ControlPanel listener) {
 		super(listener);
 	}
