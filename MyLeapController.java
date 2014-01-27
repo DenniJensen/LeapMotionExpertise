d74dec03b24b1631a4ae4fa64d353126ef6feb9c
Dennis Hägler <s0532338@htw-berlin.de>
2014-01-27 09:04:02 +0100

    Added lock method to lock the hand on the last frame. If Frames are in the same field for N frames, it will be locked and showen in the view

---
+++ MyLeapController.java
@@ -0,0 +1,11 @@
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Listener;
+
+/**
+ * Created by dennis on 1/23/14.
+ */
+public class MyLeapController extends Controller {
+	public MyLeapController(ControlPanel listener) {
+		super(listener);
+	}
+}
