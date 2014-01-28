7ac97b3f2d54a0bbfdc97ff2f0496606608e32fc
Dennis Hägler <d.haegler@hotmail.de>
2014-01-28 10:54:05 +0100

    force commit

--- LeapMotionMain.java
+++ LeapMotionMain.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class LeapMotionMain {
 	public static void main(String[] args) {
-		//PApplet.main("ProcessingSketch");
+		PApplet.main("ProcessingSketch");
 		ControlListener controlListener = new ControlListener();
 		MyLeapMotionController leapMotionController = new MyLeapMotionController(controlListener);
 		System.out.println("Press Enter to quit...");
